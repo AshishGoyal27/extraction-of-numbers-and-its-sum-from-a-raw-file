37378c10712142b71dee360ca13f64b27289ee44
Ashish Goyal <49943219+AshishGoyal27@users.noreply.github.com>
2020-03-19 17:06:46 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,9 +1,10 @@
 import re
 hand = open("data.txt").read()
 y = re.findall("[0-9]+",hand)
+print(y)                               #it return the list of value of the numbers present in file 
 p = 0
 for i in y:
     p = int(i) + p
-print(p)
+print(p)                               #it return sum of all numbers present in file.
 
 
